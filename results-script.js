bec79629d4ef588d61d28f6290bd059bc96eb641
RGPosadas <ritchelle.posadas@gmail.com>
2018-11-11 03:36:15 -0500

    rudimentary layout for demo

--- results-script.js
+++ results-script.js
@@ -1,7 +1,9 @@
 
-function loadGIF() {
-    var val = document.getElementById("searchtext").value
+// function loadGIF() {
+    // var val = document.getElementById("searchtext").value
+    var val = window.location.search;
 
+    alert(val);
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "http://api.giphy.com/v1/gifs/search?q=" + val.split(' ').join('+') + "&api_key=NhEPr6DtewNp95j4pv4TFaMbE3lHqqs9&limit=1", true);
 
@@ -9,9 +11,9 @@ function loadGIF() {
         if (this.readyState == 4 && this.status == 200) {
 
             var myArr = JSON.parse(this.responseText);
-            document.getElementById("inner").innerHTML = "<img src='" + myArr.data[0].images.original.url +"'>";
+            document.getElementById("first").innerHTML = "<img src='" + myArr.data[0].images.original.url +"'>";
             document.getElementById("second").style.backgroundImage = "url('" + myArr.data[0].images.original.url + "')";
-            //We need an if else function here to show if we approve or not
+            // We need an if else function here to show if we approve or not
             document.getElementById("second").innerHTML = "<img src='No-Nos-300x300.png'>";
             // for (x in myArr.data) {
             //     document.getElementById("inner").innerHTML += "<img src='" + myArr.data[x].images.original.url +"'>";
@@ -20,7 +22,7 @@ function loadGIF() {
     };
 
     xhttp.send();
-}               
+// }               
 
 function move(){
     var hide = document.getElementById("searchtext")
